d7e724990dc3dfc8555f0cca4a14aaf6d1601135
Diego <diego.pachon01@usa.edu.co>
2023-11-09 17:36:47 -0500

    taller corregido

--- punto1.py
+++ punto1.py
@@ -1,5 +1,3 @@
-#creación de clase
-
 class product:
     def __init__(self, precio, nombre, cantidad):
         self.nombre = nombre
@@ -7,12 +5,8 @@ class product:
         self.cantidad = cantidad
 
     def __str__(self):
-        return f"producto: {self.nombre}, precio: ${self.precio:.2f}, cantidad: {self.cantidad}"
-    return "producto: {}, precio: ${:.2f}, cantidad: {}".format(self.nombre, self.precio, self.cantidad)
+        return "producto: {}, precio: ${:.2f}, cantidad: {}".format(self.nombre, self.precio, self.cantidad)
 
-    
- #creación de metodo para añadir y actualizar la cantidad en stock del producto   
-    
 class almacen:
     def __init__(self):
         self.inv = []
@@ -20,42 +14,40 @@ class almacen:
     def agregarproducto(self, producto):
         for p in self.inv:
             if p.nombre == producto.nombre:
-                print(f"'{producto.nombre}'ya se encuentra en el almacen")
+                print(f"'{producto.nombre}' ya se encuentra en el almacen")
                 return
             
         self.inv.append(producto)
-        print(f"'{producto.nombre}'ha sido agregado al almacen")
+        print(f"'{producto.nombre}' ha sido agregado al almacen")
 
     def actualizar_cantidad(self, nombre, cantidad):
-        
         for producto in self.inv:
             if producto.nombre == nombre:
                 producto.cantidad += cantidad
-                print(f"cantidad del producto '{nombre}' actualizado. Existencias actuales: {producto.cantidad}")
+                print(f"Cantidad del producto '{nombre}' actualizado. Existencias actuales: {producto.cantidad}")
                 return
-        print(f"no hay existencias de '{nombre}' en stock ")
+        print(f"No hay existencias de '{nombre}' en stock")
 
 Almacen = almacen()
 
 while True:
-    print("\nmenú:")
+    print("\nMenú:")
     print("1. Añadir Producto")
-    print("2. mostrar el inventario")
+    print("2. Mostrar el inventario")
     print("3. Actualizar cantidad en stock")
     print("4. Salida")
-    opcion = input("pulsa el numero correspondiente para seleccionar una opción: ")
+    opcion = input("Pulsa el número correspondiente para seleccionar una opción: ")
 
     if opcion == "1":
         nombre = input("Ingrese el nombre del producto: ")
-        precio = float(input("Ingrese el precio del producto: "))
-        cantidad = int(input("Agregue la antidad en stock del producto: "))
-        nuevo_producto = product(nombre, precio, cantidad)
+        precio = float(input("Ingrese el precio del producto: "))  # Convertir a float
+        cantidad = int(input("Agregue la cantidad en stock del producto: "))
+        nuevo_producto = product(precio, nombre, cantidad)  # Poner el precio como el primer argumento
         Almacen.agregarproducto(nuevo_producto)
 
     elif opcion == "2":
-    
         if len(Almacen.inv) == 0:
-            print("No hay existencias en el almacen.")   
+            print("No hay existencias en el almacén.")   
         else:
             print("Inventario almacén:")
             for producto in Almacen.inv:
@@ -63,9 +55,8 @@ while True:
 
     elif opcion == "3":
         nombre = input("Nombre del producto: ")
-        cantidad = int(input("ingrese la cantidad que desea añadir, si desea quitar existencias ingrese el valor en formato negativo: "))
+        cantidad = int(input("Ingrese la cantidad que desea añadir, si desea quitar existencias ingrese el valor en formato negativo: "))
         Almacen.actualizar_cantidad(nombre, cantidad)
-
 
     elif opcion == "4":
         print("Hasta luego.")
@@ -73,4 +64,5 @@ while True:
 
     else:
         print("Opción no válida. Por favor, selecciona una opción válida.")
+
 
--- punto2.py
+++ punto2.py
@@ -1,29 +1,25 @@
 import pulp
 
-# Crear el problema
 problema = pulp.LpProblem("Dieta", pulp.LpMinimize)
 
-# Variables de decisión
 x1 = pulp.LpVariable("Manzanas", 0, 6, pulp.LpInteger)
-x2 = pulp.LpVariable("Plátanos", 0, 4, pulp.LpInteger)
-x3 = pulp.LpVariable("Yogur", 0, 10, pulp.LpInteger)
+x2 = pulp.LpVariable("Bananos", 0, 4, pulp.LpInteger)
+x3 = pulp.LpVariable("Yogurt", 0, 10, pulp.LpInteger)
 
-# Función objetivo
+
 problema += 0.5 * x1 + 0.4 * x2 + 1.2 * x3
 
-# Restricciones de calorías
-problema += 50 * x1 + 89 * x2 + 150 * x3 >= 2000, "Calorías"
 
-# Restricciones de proteínas
+problema += 50 * x1 + 89 * x2 + 150 * x3 >= 2000, "Cal"
+
+
 problema += 5 * x1 + 5 * x2 + 3.4 * x3 >= 80, "Proteínas"
 
-# Resolver el problema
+
 problema.solve()
 print("Estado:", pulp.LpStatus[problema.status])
-print("Cantidad óptima de manzanas a comer por dia:", x1.varValue)
-print("Cantidad óptima de plátanos a comer por dia:", x2.varValue)
-print("Cantidad óptima de yogur a comer por dia:", x3.varValue)
+print("Cantidad de manzans que se recomienda comer:", x1.varValue)
+print("Cantidad de bananos que se recomienda comer:", x2.varValue)
+print("Cantidad de yogurt que se recomienda comer:", x3.varValue)
 print("Costo total mínimo diario:", pulp.value(problema.objective))
 
-#La solución óptima sugiere que se debe consumir 6 manzanas, 4 plátanos y 9 yogures al día para satisfacer las
-#necesidades nutricionales mientras se minimiza el costo total a $15.40 diarios.
